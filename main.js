e6912da6800885cf78549f0c0170e31c7aa2e9ee
Rex Righetti <rex@zepge.com>
2024-04-26 10:05:44 -0700

    Add Character.defend

--- main.js
+++ main.js
@@ -4,27 +4,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
 class Character {
   constructor(maxHp, ac, ap) {
-    // updated parameter from HP to maxHP
-    this.maxHp = maxHp; //added new attribute to track maximum HP
-    this.hp = maxHp; //set HP to full
+    this.maxHp = maxHp;
+    this.hp = maxHp;
     this.ac = ac;
     this.ap = ap;
   }
-  hurt(amount) {
+  #hurt(amount) {
+    //Make this private.
     this.hp = this.hp - amount;
     //TODO: Update this function to handel zero health event
   }
   heal(amount) {
     this.hp = Math.min(this.hp + amount, this.maxHp);
   }
-  //If something attacks a globin, i could run goblin.hurt(5), but this doesn't reflect what is attacking the goblin or give the goblin the ability to defend.
-  //Lets add some logic for attack.
-  //attack(target:Character):void is going to allow a character to attack a character.  (this can later be adapted to attack objects or other environments if you choose)
 
   attack(target) {
-    // hero.attack(goblin) would set target as goblin.
-    const dmg = 5; //Set Damage to a constant.
-    target.hurt(5); //this would run goblin.hurt(5)
+    const dmg = 5;
+    target.defend(5); //update target.hurt(5) to target.defend(5) to allow better processing of an attack.
+  }
+  //The target may have resistances, immunities, armor, items, etc. that help avoid an attack.  We don't want to allow a method to hurt us without first processing defense
+  //At this stage defend just runs hurt, but we will be updating this soon.
+  defend(amount) {
+    this.#hurt(amount);
+    //we made hurt private by adding a # before the name. This means I can no longer call hero.hurt(5).  I now have to call hero.defend(5) and the defend method can call this.hurt(5)
+    //this allows me to still have the hurt functionality but protects me from making a mistake in the future and calling it directly without first processing defense.
   }
 }
 
